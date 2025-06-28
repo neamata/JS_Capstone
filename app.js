68a459e0741e130a1e75d63dbc264b5388ec5396
Neamata Bidiwala <bidiwalaneamata@gmail.com>
2025-06-28 15:03:31 +0530

    Created Header, Main, Function renderHeader and db.json file

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-const heading = document.getElementById("heading-2");
-console.log(heading);
+let currentUser = true;
+
+const sections = {
+    login: document.getElementById("login-section"),
+    signup: document.getElementById("signup-section"),
+
+    //task
+    //notes
+}
+
+function renderHeader() {
+    const userInfo = document.getElementById("user-info");
+    const logoutButton = document.getElementById("logout-btn");
+    const desktopNavLinks = document.getElementById("desktop-nav");
+
+    if (currentUser === null) {
+        userInfo.textContent = "";
+        logoutButton.style.display = "none";
+        desktopNavLinks.querySelector("#nav-dashboard").style.display = "none";
+        desktopNavLinks.querySelector("#nav-tasks").style.display = "none";
+        desktopNavLinks.querySelector("#nav-notes").style.display = "none";
+    }
+    else {
+        userInfo.textContent = "randon@gmail.com";
+        logoutButton.style.display = "inline";
+        desktopNavLinks.querySelector("#nav-dashboard").style.display = "inline";
+        desktopNavLinks.querySelector("#nav-tasks").style.display = "inline";
+        desktopNavLinks.querySelector("#nav-notes").style.display = "inline";
+    }
+}
+
+renderHeader();
